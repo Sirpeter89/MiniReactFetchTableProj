b68c7668d51200f65f8947cac15904a835a81bf5
Justin Wong <just2rules@gmail.com>
2021-09-30 02:03:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,69 +4,58 @@ import { useState, useEffect, useRef } from "react";
 //https://randomuser.me/api/?results=20
 
 export default function App() {
-  const [resultsArray, setResultsArray] = useState([]);
+  // const [resultsArray, setResultsArray] = useState([]);
+  const [locationHeaders, setLocationHeaders] = useState([]);
   const stringData = useRef("");
 
   async function grabData() {
     const response = await fetch("https://randomuser.me/api/?results=20");
     const rawData = await response.json();
     stringData.current = JSON.stringify(rawData, null, 2);
-    setResultsArray(rawData.results);
+    // setResultsArray(rawData.results);
+    return rawData.results;
   }
 
-  let userList = resultsArray.map((person, index) => (
-    <tr key={index}>
-      <td>{person.name.first}</td>
-      <td>{person.name.last}</td>
-      <td>
-        {person.location.street.number}
-        {person.location.street.name}
-      </td>
-      <td>{person.location.city}</td>
-      <td>{person.location.state}</td>
-      <td>{person.location.country}</td>
-      <td>{person.location.postcode}</td>
-      <td>
-        {person.location.coordinates.latitude}
-        {person.location.coordinates.longitude}
-      </td>
-      <td>
-        {person.location.timezone.offset}
-        {person.location.timezone.description}
-      </td>
-    </tr>
-  ));
+  function flattenLocationHeaders(resultsArray) {
+    //we're going to destructure to get inner keys
+    const {
+      street,
+      coordinates,
+      timezone,
+      ...restOfKeys
+    } = resultsArray[0].location;
+
+    let headersArray = [
+      "street",
+      ...Object.keys(street),
+      "coordinates",
+      ...Object.keys(coordinates),
+      "timezone",
+      ...Object.keys(timezone),
+      ...Object.keys(restOfKeys)
+    ];
+    setLocationHeaders(headersArray);
+  }
+
+  let headers = locationHeaders.map((header, idx) => <th>{header}</th>);
 
   let table = (
     <table>
       <thead>
-        <tr>
-          <th>FirstName</th>
-          <th>LastName</th>
-          <th>Street</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Country</th>
-          <th>PostCode</th>
-          <th>Coordinates</th>
-          <th>Timezone</th>
-        </tr>
-        {userList}
+        <tr>{headers}</tr>
       </thead>
     </table>
   );
 
   useEffect(() => {
-    grabData();
+    grabData().then((resultsArray) => flattenLocationHeaders(resultsArray));
   }, []);
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      {/* <pre style={{textAlign:"start"}}>
-        {stringData.current}
-      </pre> */}
+      {/* <pre style={{ textAlign: "start" }}>{stringData.current}</pre> */}
       {table}
     </div>
   );
